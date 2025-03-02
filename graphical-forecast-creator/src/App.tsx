353baf0679da030d7dcf19028f948276c3a54139
WeatherboySuper <SuperPlays1020@gmail.com>
2025-03-02 12:38:40 -0600

    Add toggle functionality for Documentation component in App

--- graphical-forecast-creator/src/App.tsx
+++ graphical-forecast-creator/src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider, useDispatch, useSelector } from 'react-redux';
 import { store } from './store';
 import ForecastMap from './components/Map/ForecastMap';
 import OutlookPanel from './components/OutlookPanel/OutlookPanel';
 import DrawingTools from './components/DrawingTools/DrawingTools';
+import Documentation from './components/Documentation/Documentation';
 import { importForecasts, markAsSaved } from './store/forecastSlice';
 import { RootState } from './store';
 import './App.css';
@@ -16,6 +17,7 @@ import 'leaflet-draw/dist/leaflet.draw.css';
 const AppContent = () => {
   const dispatch = useDispatch();
   const { outlooks, isSaved } = useSelector((state: RootState) => state.forecast);
+  const [showDocumentation, setShowDocumentation] = useState(false);
   
   // Save forecast data to localStorage
   const handleSave = () => {
@@ -64,6 +66,11 @@ const AppContent = () => {
     }
   };
   
+  // Toggle documentation visibility
+  const toggleDocumentation = () => {
+    setShowDocumentation(!showDocumentation);
+  };
+  
   // Warn before closing/refreshing if there are unsaved changes
   React.useEffect(() => {
     const handleBeforeUnload = (e: BeforeUnloadEvent) => {
@@ -83,9 +90,13 @@ const AppContent = () => {
       <header className="App-header">
         <h1>Graphical Forecast Creator</h1>
         <p className="App-subtitle">Create graphical severe weather forecasts on a CONUS map</p>
+        <button className="doc-toggle-btn" onClick={toggleDocumentation}>
+          {showDocumentation ? 'Hide Documentation' : 'Show Documentation'}
+        </button>
       </header>
       
       <main className="App-main">
+        {showDocumentation && <Documentation />}
         <DrawingTools onSave={handleSave} onLoad={handleLoad} />
         <OutlookPanel />
         <ForecastMap />
@@ -93,9 +104,6 @@ const AppContent = () => {
       
       <footer className="App-footer">
         <p>Based on Storm Prediction Center's severe weather outlooks</p>
-        <p>
-          <a href="/docs" target="_blank" rel="noopener noreferrer">Documentation</a>
-        </p>
       </footer>
     </div>
   );
