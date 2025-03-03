a0db7b4bacc65baaa00b102b0e9cbdbc8cdc18d2
WeatherboySuper <SuperPlays1020@gmail.com>
2025-03-02 23:31:35 -0600

    Add react-router-dom and implement HashRouter in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
