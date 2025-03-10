60c34959c463fc613f357f0e455454edc2ef8791
WeatherboySuper <SuperPlays1020@gmail.com>
2025-03-09 21:49:21 -0500

    fix: GFC-19 - Adjust temporary container positioning and visibility in exportMapAsImage

--- src/utils/exportUtils.ts
+++ src/utils/exportUtils.ts
@@ -34,10 +34,14 @@ export const exportMapAsImage = async (map: L.Map, title?: string): Promise<stri
       const tempContainer = document.createElement('div');
       tempContainer.style.cssText = `
         position: absolute;
-        left: -9999px;
+        top: 0px;
+        left: 0px;
         width: ${container.clientWidth}px;
         height: ${container.clientHeight}px;
-        `;
+        visibility: hidden;
+        z-index: -1000;
+        overflow: hidden;
+      `;
       document.body.appendChild(tempContainer);
       
       // Create a temporary map for export with current view
