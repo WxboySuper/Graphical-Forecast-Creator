0aab2997a8acd485f58975d33c7d834caa46498f
WeatherboySuper <SuperPlays1020@gmail.com>
2026-09-07 04:31:54 -0500

    test: cover extracted auth settings module

--- src/auth/AuthProvider.test.tsx
+++ src/auth/AuthProvider.test.tsx
@@ -5,18 +5,12 @@ import { getDoc, onSnapshot, setDoc } from 'firebase/firestore';
 import {
   applySettingsToState,
   areOverlaySettingsEqual,
-  areUserSettingsEqual,
   asRecord,
   canSyncHostedUserDocuments,
   clearDeletedAccountSession,
-  createProfilePayload,
-  createSettingsSnapshot,
   disabledAuthAction,
   extractLocalUserFromData,
   getDefaultContextValue,
-  getRemoteSeedPayload,
-  getSettingsSyncError,
-  getSettingsUpdateError,
   initLocalAuthState,
   localRefreshBetaAccess,
   localSignInWithEmail,
@@ -24,10 +18,8 @@ import {
   localSignUpWithEmail,
   localUpdateSyncedSettings,
   postLocalJson,
-  readProfileBetaAccess,
   runInitialHostedSync,
   attachHostedSettingsSubscription,
-  readRemoteSettings,
   safeParseJson,
   seedOrApplySettings,
   startSettingsSubscription,
@@ -36,6 +28,16 @@ import {
   useAuth,
 } from './AuthProvider';
 import { queueProductMetric } from '../utils/productMetrics';
+import {
+  areUserSettingsEqual,
+  createProfilePayload,
+  createSettingsSnapshot,
+  getRemoteSeedPayload,
+  getSettingsSyncError,
+  getSettingsUpdateError,
+  readProfileBetaAccess,
+  readRemoteSettings,
+} from './authSettings';
 import themeReducer from '../store/themeSlice';
 import overlaysReducer, { type OverlaysState } from '../store/overlaysSlice';
 import monitorReducer from '../store/monitorSlice';
