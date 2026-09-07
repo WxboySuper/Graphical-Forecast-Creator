5d7c08a18371434f303923cb0180bf15d8fa9a0f
WeatherboySuper <60972009+WxboySuper@users.noreply.github.com>
2026-09-07 12:12:22 -0500

    chore: remove unused CRA bootstrap artifacts (#1154)

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import './darkMode.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { store } from './store';
 import { initializeStorePersistence } from './store/persistence';
 
@@ -34,8 +33,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
