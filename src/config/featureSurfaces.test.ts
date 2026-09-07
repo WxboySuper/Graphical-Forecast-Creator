29fc1d78508f90c403fdfdeb4c9365de5714690b
WeatherboySuper <SuperPlays1020@gmail.com>
2026-09-06 22:18:42 -0500

    refactor: remove inert feature registry entries

--- src/config/featureSurfaces.test.ts
+++ src/config/featureSurfaces.test.ts
@@ -1,5 +1,4 @@
 import { GATED_ROUTE_DEFINITIONS, FEATURE_SIDE_EFFECT_MODULES } from './featureSurfaces';
-import { FEATURE_EXPOSURE_REGISTRY } from './featureExposure';
 
 describe('featureSurfaces', () => {
   test('declares lazy route loaders for gated workspace features', () => {
@@ -15,12 +14,4 @@ describe('featureSurfaces', () => {
     expect(FEATURE_SIDE_EFFECT_MODULES.customProducts).toEqual(['../lib/customProductsRepository']);
   });
 
-  test('keeps unfinished workspace exposure disabled', () => {
-    expect(FEATURE_EXPOSURE_REGISTRY.mesoscaleWorkspace.exposure).toEqual({
-      local: false, beta: false, staging: false, production: false,
-    });
-    expect(FEATURE_EXPOSURE_REGISTRY.winterWorkspace.exposure).toEqual({
-      local: false, beta: false, staging: false, production: false,
-    });
-  });
 });
