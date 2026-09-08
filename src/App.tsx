0f26396e779c36fb098317c4280ba4114fbe3fda
WeatherboySuper <SuperPlays1020@gmail.com>
2026-09-08 16:11:29 -0500

    Revert "feat(workspace): explain legacy forecast route"
    This reverts commit 5afba2c57ed77727ee5da5b5d28c0d0199cd6553.

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,6 @@ const ForecastLegacyRedirect = () => {
   return (
     <Navigate
       to={{ pathname: getDefaultForecastWorkspacePath(), search: location.search, hash: location.hash }}
-      state={{ legacyForecastRedirect: true }}
       replace
     />
   );
